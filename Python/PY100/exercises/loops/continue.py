c2ffaa1bbffd55b081afcf0f4507af3ae5ddfe7c
JosephASullivan <josepasullivan3@gmail.com>
2025-05-14 17:19:19 -0400

    add exercises

---
+++ Python/PY100/exercises/loops/continue.py
@@ -0,0 +1,17 @@
+cities = [
+    "Istanbul",
+    "Los Angeles",
+    "Tokyo",
+    None,
+    "Vienna",
+    None,
+    "London",
+    "Beijing",
+    None,
+]
+
+for city in cities:
+    if not city:
+        continue
+
+    print(len(city))
